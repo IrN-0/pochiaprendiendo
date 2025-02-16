ec92d6b3aa307b92a3a3a4db53f167fb4222cae7
IrN-0 <irenecaleroalba07@gmail.com>
2025-02-16 13:16:59 +0100

    loopsitos

---
+++ LPS1.py
@@ -0,0 +1,5 @@
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces. 
+
+palabra = str(input("introduce una palabra para que se repita 10 veces: "))
+for i in range (10):
+    print(palabra)
